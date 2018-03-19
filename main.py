16aaa90d48454952c026554c8325359ba0c6d1b3
Aude Lejuez <dusterherz@gmail.com>
2018-03-19 17:27:56 +0100

    Get departures from a specific stop

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3
-from on_time_pie import OnTimePie
 import time
+import sys
+from on_time_pie import OnTimePie
 
 if __name__ == '__main__':
-    onTimePie = OnTimePie()
+    if len(sys.argv) < 2:
+        print("You need to give the token in args")
+        sys.exit()
+    on_time_pie = OnTimePie(sys.argv[1])
     while True:
-        onTimePie.updateTime()
+        on_time_pie.update_time()
+        print(on_time_pie.next_passages)
         time.sleep(60)
